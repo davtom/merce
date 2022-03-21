0e9263f3ef5376ff1e663c7f1884f96546991929
Dawid Zamorowski <44470978+davtom@users.noreply.github.com>
2022-03-21 21:29:21 +0100

    Update activities.php

--- activities.php
+++ activities.php
@@ -6,16 +6,15 @@
 
 
         function dodaj($nazwa){
-            include 'pages/config.php'; // <- wprowadziłem w błąd, do funkcji musisz includować cały kod
-            $link->query("INSERT INTO zakupy VALUES(null,'$nazwa','N')"); //<- niewłaściwy escape string, clutchuj przed lepiej, nie wrzucał danych, najpierw stosuj "" zamiast ''
-            $link->Close(); // <- zawsze zamykaj połączenie
-            //<- location nie potrzebne, kod wywołujesz na konkretnej stronie, sama się odświeży po wykonaniu polecenia
+            include 'pages/config.php';
+            $link->query("INSERT INTO zakupy VALUES(null,'$nazwa','N')");
+            $link->Close();
         }
 
         function usun($id){
             include 'pages/config.php';
             $link->query('DELETE FROM zakupy WHERE id='.intval($id).' limit 1');
-            $link->Close();// <- zawsze zamykaj połączenie
+            $link->Close();
         }
 
         function zmienStan($id){
@@ -26,8 +25,8 @@
             } else{
                 $nw = 'T';
             }
-            $link->query("UPDATE zakupy SET stan='$nw' where id=".intval($id)." limit 1"); //<- tu miałeś mysqli_query bez kompletu
-            $link->Close();// <- zawsze zamykaj połączenie
+            $link->query("UPDATE zakupy SET stan='$nw' where id=".intval($id)." limit 1");
+            $link->Close();
         }
 
     }
